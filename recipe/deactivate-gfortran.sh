6ae01041ff6e7020a57430ed41d584ac846cc796
Duncan Macleod <duncan.macleod@ligo.org>
2020-02-04 13:52:14 +0000

    update deactivate-gfortran.sh
    to match activate-gfortran (modulo `'activate'` -> `'deactivate'`)

--- recipe/deactivate-gfortran.sh
+++ recipe/deactivate-gfortran.sh
@@ -108,11 +108,11 @@ _tc_activation \
 _tc_activation \
   deactivate host @CHOST@ @CHOST@- \
   "FC,${FC:-${GFORTRAN}}" \
-  "F77,${F77:-${GFORTRAN}}"
+  "F77,${F77:-${GFORTRAN}}" \
+  "F90,${F90:-${GFORTRAN}}"
 
 if [ $? -ne 0 ]; then
   echo "ERROR: $(_get_sourced_filename) failed, see above for details"
-#exit 1
 else
   if [ "${CONDA_BUILD:-0}" = "1" ]; then
     if [ -f /tmp/new-env-$$.txt ]; then
