bc99e5ad7f376c9ddbc9047242db099ba66acd20
Isuru Fernando <isuruf@gmail.com>
2021-07-31 03:27:42 +0530

    set CBUILD and CHOST

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,22 @@
+get_cpu_arch() {
+  local CPU_ARCH
+  if [[ "$1" == "linux-64" ]]; then
+    CPU_ARCH="x86_64"
+  elif [[ "$1" == "linux-ppc64le" ]]; then
+    CPU_ARCH="powerpc64le"
+  elif [[ "$1" == "linux-aarch64" ]]; then
+    CPU_ARCH="aarch64"
+  elif [[ "$1" == "linux-s390x" ]]; then
+    CPU_ARCH="s390x"
+  else
+    echo "Unknown architecture"
+    exit 1
+  fi
+  echo $CPU_ARCH
+}
 
-CBUILD=$(${PREFIX}/bin/*-${ctng_vendor}-linux-gnu-gcc -dumpmachine)
-CHOST=${ctng_cpu_arch}-${ctng_vendor}-linux-gnu
+export CBUILD="$(get_cpu_arch $target_platform)-${ctng_vendor}-linux-gnu"
+export CHOST="$(get_cpu_arch $ctng_target_platform)-${ctng_vendor}-linux-gnu"
 
 FINAL_CFLAGS=FINAL_CFLAGS_${ctng_target_platform_u}
 FINAL_DEBUG_CFLAGS=FINAL_DEBUG_CFLAGS_${ctng_target_platform_u}
