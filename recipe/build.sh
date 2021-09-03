67d4cf454b9fa1ba1d61c57352d304deb0958833
Isuru Fernando <isuruf@gmail.com>
2021-09-02 23:12:36 -0700

    Add symlinks

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,25 +2,7 @@
 
 set -ex
 
-get_cpu_arch() {
-  local CPU_ARCH
-  if [[ "$1" == "linux-64" ]]; then
-    CPU_ARCH="x86_64"
-  elif [[ "$1" == "linux-ppc64le" ]]; then
-    CPU_ARCH="powerpc64le"
-  elif [[ "$1" == "linux-aarch64" ]]; then
-    CPU_ARCH="aarch64"
-  elif [[ "$1" == "linux-s390x" ]]; then
-    CPU_ARCH="s390x"
-  else
-    echo "Unknown architecture"
-    exit 1
-  fi
-  echo $CPU_ARCH
-}
-
-export CBUILD="$(get_cpu_arch $target_platform)-${ctng_vendor}-linux-gnu"
-export CHOST="$(get_cpu_arch $ctng_target_platform)-${ctng_vendor}-linux-gnu"
+source $RECIPE_DIR/get_cpu_arch.sh
 
 FINAL_CFLAGS=FINAL_CFLAGS_${ctng_target_platform_u}
 FINAL_DEBUG_CFLAGS=FINAL_DEBUG_CFLAGS_${ctng_target_platform_u}
