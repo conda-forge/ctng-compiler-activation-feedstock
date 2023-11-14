6ffcae7e907bf1727b883c369b9f6470aa319317
Isuru Fernando <isuruf@gmail.com>
2023-11-14 09:21:37 -0600

    create dir

--- recipe/install-clang.sh
+++ recipe/install-clang.sh
@@ -6,9 +6,10 @@ cp "${SRC_DIR}"/deactivate-clang.sh ${PREFIX}/etc/conda/deactivate.d/deactivate-
 
 if [[ "$target_platform" == linux-* && "$target_platform" != "$cross_target_platform" ]]; then
   # Set clang symlink with target
-  ln -sf $CONDA_PREFIX/bin/clang $CONDA_PREFIX/bin/${CHOST}-clang
+  mkdir -p ${CONDA_PREFIX}/bin
+  ln -sf ${CONDA_PREFIX}/bin/clang ${CONDA_PREFIX}/bin/${CHOST}-clang
   # Set sysroot. The other option is to use `CONDA_BUILD_SYSROOT` env variable
   # but this is better than using an env variable.
-  echo "--sysroot $CONDA_PREFIX/${CHOST}/sysroot" >> $CONDA_PREFIX/bin/${CHOST}-clang.cfg
+  echo "--sysroot ${CONDA_PREFIX}/${CHOST}/sysroot" >> ${CONDA_PREFIX}/bin/${CHOST}-clang.cfg
 fi
 
