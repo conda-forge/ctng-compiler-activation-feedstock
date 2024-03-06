149c09c22397045622cd531238db33120e7518ef
H. Vetinari <h.vetinari@gmx.com>
2024-03-06 13:35:55 +1100

    match install-clang++.sh with install-clang.sh

--- recipe/install-clang++.sh
+++ recipe/install-clang++.sh
@@ -1,9 +1,14 @@
+source $RECIPE_DIR/get_cpu_arch.sh
+
 mkdir -p ${PREFIX}/etc/conda/{de,}activate.d
 cp "${SRC_DIR}"/activate-clang++.sh ${PREFIX}/etc/conda/activate.d/activate-${PKG_NAME}.sh
 cp "${SRC_DIR}"/deactivate-clang++.sh ${PREFIX}/etc/conda/deactivate.d/deactivate-${PKG_NAME}.sh
 
-mkdir -p ${PREFIX}/bin
 if [[ "$target_platform" != "$cross_target_platform" ]]; then
-  ln -sf ${PREFIX}/bin/clang++ ${PREFIX}/bin/${ctng_cpu_arch}-conda-linux-gnu-clang++
-  echo "--sysroot ${PREFIX}/${ctng_cpu_arch}-conda-linux-gnu/sysroot" >> ${PREFIX}/bin/${ctng_cpu_arch}-conda-linux-gnu-clang++.cfg
+  # Set clang++ symlink with target
+  mkdir -p ${PREFIX}/bin
+  ln -sf ${PREFIX}/bin/clang++ ${PREFIX}/bin/${CHOST}-clang++
+  # Set sysroot. The other option is to use `CONDA_BUILD_SYSROOT` env variable
+  # but this is better than using an env variable.
+  echo "--sysroot ${PREFIX}/${CHOST}/sysroot" >> ${PREFIX}/bin/${CHOST}-clang++.cfg
 fi
