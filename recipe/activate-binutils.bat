a6f127d31abb642bfee8af3b6f2c3d5d756e2433
Isuru Fernando <isuruf@gmail.com>
2024-09-18 09:25:58 -0500

    Fix binutils activation

--- recipe/activate-binutils.bat
+++ recipe/activate-binutils.bat
@@ -1,6 +1,6 @@
 @echo off
 set "CONDA_BACKUP_AR=%AR%"
-set "AR=%CHOST%-ar.exe"
+set "AR=@CHOST@-ar.exe"
 
 set "CONDA_BACKUP_RANLIB=%RANLIB%"
-set "RANLIB=%CHOST%-ranlib.exe"
+set "RANLIB=@CHOST@-ranlib.exe"
