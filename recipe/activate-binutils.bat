54720095d38e6dabdcb27350f58b14f1a7a18390
Peter Williams <peter@newton.cx>
2024-09-25 10:50:30 -0400

    Improve Windows binutils activation for Autotools builds
    Standard autotools builds check for a bunch of additional binutils
    programs that we provide under special names, namely: as, dlltool, nm,
    objdump, and strip. Provide additional environment variables for these.
    This fixes libtool's ability to figure out which static libraries are
    DLL import stubs on Windows, as work around in
    https://github.com/conda-forge/xorg-libxdmcp-feedstock/pull/14 .

--- recipe/activate-binutils.bat
+++ recipe/activate-binutils.bat
@@ -2,5 +2,20 @@
 set "CONDA_BACKUP_AR=%AR%"
 set "AR=@CHOST@-ar.exe"
 
+set "CONDA_BACKUP_AS=%AS%"
+set "AS=@CHOST@-as.exe"
+
+set "CONDA_BACKUP_DLLTOOL=%DLLTOOL%"
+set "DLLTOOL=@CHOST@-dlltool.exe"
+
+set "CONDA_BACKUP_NM=%NM%"
+set "NM=@CHOST@-nm.exe"
+
+set "CONDA_BACKUP_OBJDUMP=%OBJDUMP%"
+set "OBJDUMP=@CHOST@-objdump.exe"
+
 set "CONDA_BACKUP_RANLIB=%RANLIB%"
 set "RANLIB=@CHOST@-ranlib.exe"
+
+set "CONDA_BACKUP_STRIP=%STRIP%"
+set "STRIP=@CHOST@-strip.exe"
