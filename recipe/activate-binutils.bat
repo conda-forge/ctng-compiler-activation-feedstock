e64bea49c7a9f8efd99662feb18dad641adac8fa
Brian Ward <brianmward99@gmail.com>
2025-02-20 17:23:39 -0500

    Remove other binutils files

--- recipe/activate-binutils.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set "CONDA_BACKUP_AR=%AR%"
-set "AR=@CHOST@-ar.exe"
-
-set "CONDA_BACKUP_RANLIB=%RANLIB%"
-set "RANLIB=@CHOST@-ranlib.exe"
--- recipe/deactivate-binutils.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set "AR=%CONDA_BACKUP_AR%"
-set "CONDA_BACKUP_AR="
-
-set "RANLIB=%CONDA_BACKUP_RANLIB%"
-set "CONDA_BACKUP_RANLIB="
