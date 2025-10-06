2d8f7bb5933ca60663c117707516b5c3cbcd36b7
Isuru Fernando <isuruf@gmail.com>
2025-10-06 09:03:06 -0500

    convert to v1

--- recipe/install-gfortran.sh
+++ recipe/install-gfortran.sh
@@ -1,3 +1,4 @@
+source ${RECIPE_DIR}/build_scripts.sh
 mkdir -p ${PREFIX}/etc/conda/{de,}activate.d
 cp "${SRC_DIR}"/activate-gfortran.sh ${PREFIX}/etc/conda/activate.d/activate-${PKG_NAME}.sh
 cp "${SRC_DIR}"/deactivate-gfortran.sh ${PREFIX}/etc/conda/deactivate.d/deactivate-${PKG_NAME}.sh
